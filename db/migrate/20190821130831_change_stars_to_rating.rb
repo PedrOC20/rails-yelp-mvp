221d4b1d2c3c7acaa4f261df39ece912b83f41a8
Pedro Candeias <pedromiguelcandeias@gmail.com>
2019-08-21 14:15:13 +0100

    Rails with nest Restaurant and Reviw Models

---
+++ db/migrate/20190821130831_change_stars_to_rating.rb
@@ -0,0 +1,5 @@
+class ChangeStarsToRating < ActiveRecord::Migration[5.2]
+  def change
+    rename_column :reviews, :stars, :rating
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+
+Restaurant.create(name: 'Ramiro', address: 'Lisbon', phone_number: '915862314', category: 'chinese')
+Restaurant.create(name: 'Ramiro 1', address: 'Porto', phone_number: '965862314', category: 'italian')
+Restaurant.create(name: 'Ramiro 2', address: 'Madrid', phone_number: '935862314', category: 'japanese')
+Restaurant.create(name: 'Ramiro 3', address: 'Barcelona', phone_number: '925862314', category: 'french')
+Restaurant.create(name: 'Ramiro 4', address: 'Hawai', phone_number: '945862314', category: 'belgian')
